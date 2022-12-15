cb597c3b054ecb899be343d3918048bc972780c3
mason-yin <mason.yin@cybercrucible.com>
2022-12-15 01:09:56 -0500

    A4 is complete, ready for review

--- client/get_book_titles.py
+++ client/get_book_titles.py
@@ -4,20 +4,23 @@ import service.A3_pb2 as pb2
 import service.A3_pb2_grpc as pb2_grpc
 
 
-def getbook(client, isbn_list):
-    server_stub = client.inventory_stub()
-    res = []
-    for i in range(len(isbn_list)):
-        isbn = isbn_list[i]
-        request = pb2.GetBookByISBNRequest(ISBN=isbn)
-        response = server_stub.GetBookByISBN(request)
-        res.append(response.book.title)
-    return res
+class GetBook:
+    def get_book_titles(self, client, isbn_list):
+        server_stub = client.inventory_stub()
+        res = []
+        for i in range(len(isbn_list)):
+            isbn = isbn_list[i]
+            request = pb2.GetBookByISBNRequest(ISBN=isbn)
+            response = server_stub.GetBookByISBN(request)
+            res.append(response.book.title)
+        return res
 
 
 if __name__ == '__main__':
+    # start the server in server.py!!!
     client = Client('localhost', 50051)
+    get_book = GetBook()
     isbn_list = ["9789775325822", "9780155658110"]
-    res = getbook(client, isbn_list)
+    res = get_book.get_book_titles(client, isbn_list)
     for i in range(len(res)):
         print(res[i])
--- client/test.py
+++ client/test.py
@@ -1,20 +1,38 @@
-from unittest.mock import patch, Mock
-from unittest import TestCase
+from unittest.mock import MagicMock
+from unittest import TestCase, mock
 
-from client.get_book_titles import getbook
+from client.get_book_titles import GetBook
 from client.inventory_client import Client
-from service.server import start
+
+mock_client = MagicMock()
+get_book = GetBook()
+
+
+def my_side_effect(*args):
+    dic = {"9780155658110": "Nineteen Eighty-Four", "9780582060104": "Animal Farm",
+           "9780395647400": "The Lord of Rings", "9789775325822": "Ozymandias"}
+    res = []
+    isbn_list = args[1]
+    for i in range(len(isbn_list)):
+        res.append(dic[isbn_list[i]])
+    return res
 
 
 class TestGetTitles(TestCase):
-    @patch('inventory_client.Client')
-    def test_mock(self, mock_client):
-        client = mock_client()
+
+    def test_mock(self):
+        isbn_list = ["9780155658110", "9780395647400"]
+        with mock.patch.object(GetBook, 'get_book_titles', side_effect=my_side_effect):
+            res = get_book.get_book_titles(mock_client, isbn_list)
+            desired = ["Nineteen Eighty-Four", "The Lord of Rings"]
+            self.assertIsNotNone(res)
+            self.assertListEqual(res, desired)
 
     def test_live(self):
+        # go to server.py to start the server before running this test
         client = Client('localhost', 50051)
         isbn_list = ["9789775325822", "9780155658110"]
-        res = getbook(client, isbn_list)
-        answer = ["Ozymandias", "Nineteen Eighty-Four"]
+        res = get_book.get_book_titles(client, isbn_list)
+        desired = ["Ozymandias", "Nineteen Eighty-Four"]
         self.assertIsNotNone(res)
-        self.assertListEqual(res, answer)
+        self.assertListEqual(res, desired)
